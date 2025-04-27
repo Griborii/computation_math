96db37513b2d3e77ca66cf172d033878bdb28a66
Grigoriy <grao319@yandex.ru>
2025-04-27 16:50:48 +0300

    project, add theor wave solvetion

--- project/1d_wave.py
+++ project/1d_wave.py
@@ -4,6 +4,7 @@ from IPython.display import HTML
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import linalg
 
 def explicit_runge_kutt(n, h, start, start_val, func):
     a = [
@@ -30,33 +31,18 @@ def explicit_runge_kutt(n, h, start, start_val, func):
         val += h * sum(np.array([b[i]*dif[i] for i in range(len(dif))]))
     return vals
 
-def gaus(x):
-    d = 0.1
-    return math.exp(-((x - 0.5) / d) ** 2)
-
-def wave(func, dt, dx):
+def wave(g, H, func, dt, dx, t_start, t_end, x_start, x_end):
     # калибровка длины шага, чтобы из вмещалось целое число
-    t_start = 0
-    t_end = 1
-    x_start = 0
-    x_end = 1
     N_t = int((t_end - t_start) / dt)
     dt = (t_end - t_start) / N_t
     N_x = int((x_end - x_start) / dx)
     dx = (x_end - x_start) / N_x
+    c = math.sqrt(g * H)
 
     # начальный вектор значений функции при x от 0 до 1 при t = 0
-    x = [x_start + i * dx for i in range(N_t + 1)]
-    f_0 = [[0 for i in range(N_t + 1)], [func(x[i]) for i in range(N_t + 1)]]
-    
-    # анимация
-    fig, ax = plt.subplots()
-    line, = ax.plot(x, f_0[1])  # Инициализируем линию графика
-    ax.set_xlim(0, 1)  # Задаём пределы по x
-    ax.set_ylim(0, 1)  # Задаём пределы по y (важно для стабильности)
-    ax.set_xlabel("x")
-    ax.set_ylabel("f(x, t)")
-    ax.set_title("Анимация функции f(x, t)")
+    x = [x_start + i * dx for i in range(N_x + 1)]
+    t = [t_start + i * dx for i in range(N_t + 1)]
+    f_0 = [[0 for i in range(N_x + 1)], [func(x[i]) for i in range(N_x + 1)]]
 
     # вместо правой части производная по координате
     def minus_x_diff(vec, t):
@@ -69,23 +55,84 @@ def wave(func, dt, dx):
         diff[1][-1] = diff[1][0]
         for i in range(1, len(vec[0]) - 1):
             diff[1][i] = (vec[0][i + 1] - vec[0][i - 1]) / (2 * dx)
-        return np.array([-diff[0], -diff[1]])
-
-    def animate(ind):
-        """Функция, вызываемая на каждом кадре анимации."""
-        y = rk_res[int(ind)][1] # Вычисляем новое значение функции
-        line.set_ydata(y)      # Обновляем данные линии графика
-        return line,           # Важно вернуть итерируемый объект (tuple)
+        return np.array([-diff[0] * g, -diff[1] * H])
 
     # МРК 4 для шага по времени
     # вместо правой части в explicit_runge_kutt кидаем производную по координате
-    rk_res =  explicit_runge_kutt(N_t, dt, t_start, f_0, minus_x_diff)
+    rk_res =  explicit_runge_kutt(N_t, dt, t_start, f_0, minus_x_diff) 
+    return {"result": {"values": rk_res, "x_grid": x, "t_grid": t}}
+
+def wave_animation(rk_val, x_grid):
+    # анимация
+    fig, ax = plt.subplots()
+    line, = ax.plot(x_grid, rk_val[0])  # Инициализируем линию графика
+    ax.set_xlim(0, 1)  # Задаём пределы по x
+    ax.set_ylim(0, 1)  # Задаём пределы по y (важно для стабильности)
+    ax.set_xlabel("x")
+    ax.set_ylabel("f(x, t)")
+    ax.set_title("Анимация функции f(x, t)")
+
+    def animate(ind):
+        """Функция, вызываемая на каждом кадре анимации."""
+        y = rk_val[int(ind)] # Вычисляем новое значение функции
+        line.set_ydata(y)      # Обновляем данные линии графика
+        return line,           # Важно вернуть итерируемый объект (tuple)
 
     ani = animation.FuncAnimation(
-        fig, animate, frames=np.linspace(0, len(rk_res) - 1, len(rk_res) - 1),  # 100 кадров от 0 до 20
+        fig, animate, frames=np.linspace(0, len(rk_val) - 1, len(rk_val) - 1),  # 100 кадров от 0 до 20
         interval=50, blit=False, repeat=True
     )
-
     plt.show()
+    
+def wave_eq_solve(c, start_func, x_grid, t_grid):
+    theor_vals = []
+    for i in range(len(t_grid)):
+        theor_vals.append(np.zeros(len(x_grid)))
+        t = t_grid[i]
+        for j in range(len(x_grid)):
+            x = x_grid[j]
+            theor_vals[i][j] = (start_func(x + c * t) + start_func(x - c * t)) / 2
+    return np.array(theor_vals)
 
-wave(gaus, 0.01, 0.01)
+def func_sim(func, x_left, x_right):
+    def new_func(x):
+        T = x_right - x_left
+        x_sim = x - math.floor((x - x_left) / T) * T
+        return gaus(x_sim)
+    
+    return new_func
+
+def gaus(x):
+    d = 0.1
+    return math.exp(-((x - 0.5) / d) ** 2)
+
+dt = 0.001
+dx = 0.001
+t_start = 0
+t_end = 1
+x_start = 0
+x_end = 1
+
+# калибровка длины шага, чтобы из вмещалось целое число
+N_t = int((t_end - t_start) / dt)
+dt = (t_end - t_start) / N_t
+N_x = int((x_end - x_start) / dx)
+dx = (x_end - x_start) / N_x
+
+start_func = gaus
+g = 5
+H = 1
+c = math.sqrt(g * H)
+
+res = wave(g, H, start_func, dt, dx, t_start, t_end, x_start, x_end)
+vals, x_grid, t_grid = res["result"]["values"], res["result"]["x_grid"], res["result"]["t_grid"]
+h_vals = []
+for i in range(len(vals)):
+    h_vals.append(np.copy(vals[i][1]))
+
+# wave_animation(vals, x_grid)
+theor_vals = wave_eq_solve(c, func_sim(start_func, x_start, x_end), x_grid, t_grid)
+err = [np.max(h_vals[i] - theor_vals[i]) for i in range(len(t_grid))]
+# wave_animation(h_vals, x_grid)
+plt.plot(t_grid, err)
+plt.show()
